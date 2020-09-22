f6588a3d98273a8dfd1f2dd6eb8358b41c170f3e
James Walsh <walshja03@gmail.com>
2020-09-22 16:16:07 -0400

    clean up files

--- static/js/load.js
+++ static/js/load.js
@@ -1,4 +1,19 @@
-var tableData = data;
+
+var dateselected = ""
+var dateSel = new Litepicker({
+    element: document.getElementById('datepicker'),
+    startDate:"2010-01-01",
+    maxDate: "2010-01-13",
+    minDate: "2010-01-01",
+    format: 'YYYY-MM-DD',
+    onSelect: function (date1){
+        console.log(date1)
+        dateselected =dateSel.getDate('YYYY-MM-DD')
+        return dateselected
+    }
+    
+})
+tableData = data
 
 //Populate table with all data on initial load
 tableData.forEach(function(ufoReport){
@@ -9,6 +24,8 @@ tableData.forEach(function(ufoReport){
         row.append("td").text(v);
     })
 })
+//date picker
+
 
 // function to convert strings to title case.  Credit: // https://gist.github.com/SonyaMoisset/aa79f51d78b39639430661c03d9b1058#file-title-case-a-sentence-for-loop-wc-js
 var toTitleCase = function (str) {
@@ -23,9 +40,12 @@ function uniqueValues(value, index, self){
     return self.indexOf(value) === index
 }
 
+
+
 // map header values including city,state,country, and shape to a unique list for 
 // drop down selection options
 var dates = tableData.map(siting => siting.datetime).filter(uniqueValues);
+console.log(dates)
 var sts = tableData.map(siting => siting.state.toUpperCase()).filter(uniqueValues).sort();
 var cities = tableData.map(function(siting) {
     a = toTitleCase(siting.city)
@@ -33,12 +53,12 @@ var cities = tableData.map(function(siting) {
 }).filter(uniqueValues).sort();
 var country = tableData.map(siting =>siting.country.toUpperCase()).filter(uniqueValues).sort();
 var shape = tableData.map(siting =>siting.shape).filter(uniqueValues).sort();
-console.log(dates)
+// console.log(dates)
 //Load Initial Filter Values
-dates.forEach(function(c) {
-    var x = d3.select("#dateSelect");
-    x.append("option").text(c).attr("value", c);
-})
+// dates.forEach(function(c) {
+//     var x = d3.select("#dateSelect");
+//     x.append("option").text(c).attr("value", c);
+// })
 country.forEach(function(c) {
     var x = d3.select("#countrySelect");
     x.append("option").text(c).attr("value", c.toLowerCase());
